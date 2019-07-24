92307986b21cf37f044ec63b89c913766130b515
Alex Combas <alex@flyingcastle.net>
2019-07-23 20:59:58 -0700

    Comlete DigitSum exercise

--- src/section/five/DigitSum.java
+++ src/section/five/DigitSum.java
@@ -2,20 +2,25 @@ package section.five;
 
 public class DigitSum {
     public static void main(String[] args) {
-
+        System.out.println(sumDigits(125));
+        System.out.println(sumDigits(2048));
     }
 
-    public static int sumDigits(int number) {
-        if (number < 10) {
-            return -1;
-        }
+    // break number into individual digits and add them together
+    // sumDigits(125) -> 1 + 2 + 5 = 8
+    // sumDigits(2048) -> 2 + 0 + 4 + 8 = 14
+    private static int sumDigits(int n) {
         int sum = 0;
-        for (int i = 1; i <= number; i++) {
-           sum += i;
+        int number = n;
 
+        // values less than 10 are invalid
+        if (number < 10) { return -1; }
+
+        // extract the final digit then accumulate
+        while (number > 0) {
+            sum += (number % 10);
+            number = (int) Math.floor(number / 10);
         }
         return sum;
     }
-
-
 }
