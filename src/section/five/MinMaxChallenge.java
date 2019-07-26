3a4f206040a50aafebfaeb6c47e44d59eb772294
Alex Combas <alex@flyingcastle.net>
2019-07-26 11:15:54 -0700

    Finished Min and Max challenge
    Signed-off-by: Alex Combas <alex@flyingcastle.net>

---
+++ src/section/five/MinMaxChallenge.java
@@ -0,0 +1,37 @@
+package section.five;
+
+import java.util.Scanner;
+
+public class MinMaxChallenge {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int min = 0;
+        int max = 0;
+        int temp = 0;
+        boolean hasNext;
+        while (true) {
+            System.out.print("Enter number: ");
+            hasNext = scanner.hasNextInt();
+            if (hasNext) {
+               temp = scanner.nextInt();
+               scanner.nextLine();
+               if (temp < 1) {
+                   System.out.println("Minimum number entered: " + min);
+                   System.out.println("Maximum number entered: " + max);
+                   break;
+               }
+
+               if (temp < min || min == 0) {
+                   min = temp;
+               } else if (temp > max || max == 0) {
+                   max = temp;
+               }
+
+            }
+        }
+
+
+
+    }
+}
